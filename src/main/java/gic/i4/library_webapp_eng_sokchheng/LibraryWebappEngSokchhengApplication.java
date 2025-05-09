8a7734baf1d048c1fb86de4c827150df6fd82909
ChhenG02 <itcdev@edu.kh>
2025-05-09 11:17:09 +0700

    first commit

---
+++ src/main/java/gic/i4/library_webapp_eng_sokchheng/LibraryWebappEngSokchhengApplication.java
@@ -0,0 +1,13 @@
+package gic.i4.library_webapp_eng_sokchheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWebappEngSokchhengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryWebappEngSokchhengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gic/i4/library_webapp_eng_sokchheng/LibraryWebappEngSokchhengApplicationTests.java
@@ -0,0 +1,13 @@
+// package gic.i4.library_webapp_eng_sokchheng;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class LibraryWebappEngSokchhengApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
